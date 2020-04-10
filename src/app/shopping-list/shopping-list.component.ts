58d8d0f2882c05e47556380a45e961e8d489d4f8
Daniel McIntyre <dmcintyre25@gmail.com>
2020-04-10 06:36:23 -0400

    updated shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,6 +14,10 @@ export class ShoppingListComponent implements OnInit {
   ]
   constructor() { }
 
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
   ngOnInit(): void {
   }
 
